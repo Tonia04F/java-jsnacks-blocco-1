4d9580a33ec8cf7475ed668be38fa653a03318d6
tonia <tonia@cia.como>
2022-06-13 10:48:53 +0200

    first snack done

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -1,9 +1,32 @@
 package jsnacks;
 
+import java.util.Scanner;
+
 public class JSnack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Il software deve chiedere per 5 volte all’utente di inserire un numero.
+			Il programma stampa la somma di tutti i numeri inseriti.
+			Esegui questo programma in due versioni, con il for e con il while.*/ 
+		
+		
+	Scanner sc = new Scanner(System.in);
+		System.out.println("inserisci 5 numeri: ");
+		
+		
+		int numero;
+	    int somma = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			numero = sc.nextInt();
+		    somma = somma + numero;
+		
+			
+		}
+		System.out.println("la somma dei 5 numeri è: " + somma);
+		
+		
+		
 
 	}
 
